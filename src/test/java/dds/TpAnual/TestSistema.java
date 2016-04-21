badd0bc018516ff7d4c4c70feb47c6008d8c036d
mbondarenco <maru.bondarenco@gmail.com>
2016-04-21 02:31:30 -0300

    Agrego el test de Sistema

---
+++ src/test/java/dds/TpAnual/TestSistema.java
@@ -0,0 +1,54 @@
+package dds.TpAnual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbar.geodds.Point;
+
+import TpAnual.Banco;
+import TpAnual.Domicilio;
+import TpAnual.ParadaColectivo;
+import TpAnual.Poi;
+import TpAnual.Region;
+import TpAnual.Sistema;
+
+public class TestSistema {
+	private Region regionParada;
+	private Domicilio domicilioParada;
+	private Point coordenadaParada;
+	private ParadaColectivo parada114;
+	private Domicilio domicilioBanco;
+	private Region regionBanco;
+	private Point coordenadaBanco;
+	private Banco bancoSantander;
+
+	@Before
+	public void initialize(){
+		domicilioParada = new Domicilio("Arenales",1141 , "Junin", "Santa Fe", 2100, 0, 0, 0, 1111);
+		regionParada = new Region("CABA", "Recoleta", "Bs As", "Argentina");
+		coordenadaParada = new Point(34.4353,25.4632);
+		parada114 = new ParadaColectivo(domicilioParada, regionParada, coordenadaParada);
+		parada114.setLinea("Parada 114");
+		
+		domicilioBanco = new Domicilio("Arenales",1245 , "M.T.De.Alvear", "Santa Fe", 2100, 0, 0, 0, 1111);
+		regionBanco = new Region("CABA", "Recoleta", "Bs As", "Argentina");
+		coordenadaBanco = new Point(34.3243,24.4657);
+		bancoSantander = new Banco(domicilioBanco, regionBanco, coordenadaBanco);
+	}
+
+	@Test
+	public void busquedaDePoisPorClave(){
+		
+		Sistema sistema = new Sistema();
+		sistema.pois.add(parada114);
+		sistema.pois.add(bancoSantander);
+		
+		Assert.assertEquals(1, sistema.busquedaDePuntos("Cajero").size());
+		
+	}
+	
+	
+}
