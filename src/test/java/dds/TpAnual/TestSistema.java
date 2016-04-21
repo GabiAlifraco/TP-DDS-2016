7d31601d0b3ae7102a074c8c0504bf138a68ea88
mbondarenco <maru.bondarenco@gmail.com>
2016-04-21 20:51:56 -0300

    Mejoré el test de buscarPuntos

--- src/test/java/dds/TpAnual/TestSistema.java
+++ src/test/java/dds/TpAnual/TestSistema.java
@@ -27,33 +27,35 @@ public class TestSistema {
 	private Point coordenadaBanco;
 	private Banco bancoSantander;
 	private Disponibilidad disponibilidadBancaria;
-	private List<String> diasDeAtencionBancaria = Arrays.asList("Lunes","Martes","Miercoles","Jueves","Viernes");
+	private List<String> diasDeAtencionBancaria = Arrays.asList("Lunes", "Martes", "Miercoles", "Jueves", "Viernes");
 
 	@Before
-	public void initialize(){
-		domicilioParada = new Domicilio("Arenales",1141 , "Junin", "Santa Fe", 2100, 0, 0, 0, 1111);
+	public void initialize() {
+		domicilioParada = new Domicilio("Arenales", 1141, "Junin", "Santa Fe", 2100, 0, 0, 0, 1111);
 		regionParada = new Region("CABA", "Recoleta", "Bs As", "Argentina");
-		coordenadaParada = new Point(34.4353,25.4632);
-		parada114 = new ParadaColectivo(domicilioParada, regionParada, coordenadaParada,"114");
+		coordenadaParada = new Point(34.4353, 25.4632);
+		parada114 = new ParadaColectivo(domicilioParada, regionParada, coordenadaParada, "114");
 		parada114.setLinea("Parada 114");
-		
-		domicilioBanco = new Domicilio("Arenales",1245 , "M.T.De.Alvear", "Santa Fe", 2100, 0, 0, 0, 1111);
+
+		domicilioBanco = new Domicilio("Arenales", 1245, "M.T.De.Alvear", "Santa Fe", 2100, 0, 0, 0, 1111);
 		regionBanco = new Region("CABA", "Recoleta", "Bs As", "Argentina");
-		coordenadaBanco = new Point(34.3243,24.4657);
-		disponibilidadBancaria = new Disponibilidad ("10:00","15:00");
-		bancoSantander = new Banco("Banco Santander",domicilioBanco, regionBanco, coordenadaBanco,diasDeAtencionBancaria,disponibilidadBancaria);
+		coordenadaBanco = new Point(34.3243, 24.4657);
+		disponibilidadBancaria = new Disponibilidad("10:00", "15:00");
+		bancoSantander = new Banco("Banco Santander", domicilioBanco, regionBanco, coordenadaBanco,
+				diasDeAtencionBancaria, disponibilidadBancaria);
 	}
 
 	@Test
-	public void busquedaDePoisPorClave(){
-		
+	public void busquedaDePoisPorClave() {
+
 		Sistema sistema = new Sistema();
 		sistema.pois.add(parada114);
 		sistema.pois.add(bancoSantander);
-		
-		Assert.assertEquals(1, sistema.busquedaDePuntos("Cajero").size());
-		
+
+		List<Poi> resultadoEsperado = Arrays.asList(bancoSantander);
+
+		Assert.assertEquals(true, sistema.busquedaDePuntos("Cajero").equals(resultadoEsperado));
+
 	}
-	
-	
+
 }
