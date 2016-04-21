704dbe8ce8ad937b96597f47b703daf8f6328e90
mbondarenco <maru.bondarenco@gmail.com>
2016-04-21 02:22:21 -0300

    Agrego el metodo textoIncluido

--- src/main/java/TpAnual/Poi.java
+++ src/main/java/TpAnual/Poi.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.uqbar.geodds.Point;
 
 public interface Poi {
-	
-	   public boolean poiCercanoAOtro(Point otraCoordenada);
-		public boolean poiEstaDisponible();
-		public List<Poi> busquedaDePuntos(String unaBusqueda);
-		
+	public boolean poiCercanoAOtro(Point otraCoordenada);
+
+	public boolean poiEstaDisponible();
+
+	boolean textoIncluido(String texto);
 }
