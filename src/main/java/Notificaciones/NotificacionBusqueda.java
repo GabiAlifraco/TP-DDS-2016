cd8dfb4517e80f98bdde7b3ddaf5f76cf623010d
GabiAlifraco <gabriel.alifraco@gmail.com>
2016-07-01 14:36:03 -0300

    Dejo en limpio + comentarios en el paquete Notificaciones

--- src/main/java/Notificaciones/NotificacionBusqueda.java
+++ src/main/java/Notificaciones/NotificacionBusqueda.java
@@ -1,10 +1,9 @@
 package Notificaciones;
-
 import Resultado.Resultado;
 
 public interface NotificacionBusqueda {
-
+	
 	public void notificarBusqueda(Resultado resultado);
-
 	public void setTiempoMaximoBusqueda(long tiempoMaximoBusqueda);
+	
 }
