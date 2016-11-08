aaf1c2121a031796a912a09f963c727910679c1a
mbondarenco <maru.bondarenco@gmail.com>
2016-11-08 13:12:54 -0300

    Merge branch 'master' of https://github.com/dds-utn/2016-vn-group-10.git

---
+++ src/main/java/CaracteristicaPoi/Zona.java
@@ -0,0 +1,52 @@
+package CaracteristicaPoi;
+
+import java.util.List;
+
+import org.uqbar.geodds.Point;
+import org.uqbar.geodds.Polygon;
+
+import javax.persistence.Embedded;
+
+@javax.persistence.Embeddable
+public class Zona {
+
+	@Embedded
+	private List<Punto> zona;
+
+	@org.mongodb.morphia.annotations.Transient
+	@javax.persistence.Transient
+	private Polygon polygon;
+	
+	public Zona(List<Punto> puntos){
+		this.zona = puntos;
+		this.polygon = this.mapperZonaAPolygon(puntos);
+	}
+	public Zona(){
+
+	}
+	
+	public boolean isInside(Punto otraCoordenada) {
+		Point pointCoordenada = new Point(otraCoordenada.getLatitud(), otraCoordenada.getLongitud());
+		return polygon.isInside(pointCoordenada);
+	}
+	
+	private Polygon mapperZonaAPolygon(List<Punto> puntos){
+		Polygon polygon = new Polygon();
+		Point point;
+		for (Punto punto: puntos){
+			point = new Point(punto.getLatitud(), punto.getLongitud());
+			polygon.add(point);
+		}
+		return polygon;
+	}
+	
+
+	public List<Punto> getZona() {
+		return zona;
+	}
+
+	public void setZona(List<Punto> zona) {
+		this.zona = zona;
+	}
+
+}
