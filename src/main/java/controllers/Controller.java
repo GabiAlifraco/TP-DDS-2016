2c57ab632e98b0b7fb34d91bfa4a932658be16cd
mbondarenco <maru.bondarenco@gmail.com>
2016-11-15 00:13:26 -0300

    Mejoro la seguridad hasheando el valor de la cookie role

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -7,11 +9,11 @@ import spark.Response;
 public abstract class Controller {
 
 	public ModelAndView redirigirSegunPermisos(Request request, Response response, String role, ModelAndView model) {
-		if (request.cookies().containsKey("role") && request.cookie("role").toLowerCase().equals(role)) {
+		if (request.cookies().containsKey("role") && request.cookie("role").equals(DigestUtils.sha1Hex(role))) {
 			return model;
 		} else {
 			response.status(401);
-			return new ModelAndView(null, "/access-denied.hbs");
+			return new ModelAndView(null, "access-denied.hbs");
 			//return model;  Para probar las pantallas comento codigo anterior y muestro pantalla asi no me restringe el login
 		}
 
