a53aeec780e3f405bb690e8eada77440f90f105b
mbondarenco <maru.bondarenco@gmail.com>
2016-11-15 23:27:41 -0300

    Agrego cambios en login

--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -9,7 +9,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class Routes {
 	public static void main(String[] args) {
 		
-		new Bootstrap().run();
+		//new Bootstrap().run();
 		System.out.println("Iniciando servidor");
 		staticFileLocation("/public");
 		HomeController home = new HomeController();
@@ -25,7 +25,6 @@ public class Routes {
 		get("/login", accesos::mostrarLogin, engine); //Se muestra cuando presiono en el boton de la pagina principal
 		post("/login", accesos::autenticar); //Sucede cuando presionamos iniciar sesion luego de ingresar los datos
 		get("/loginfailed", accesos::mostrarLoginFail, engine); //En caso que haya un error en los datos
-		post("/loginfailed", accesos::autenticar); //Creo que no lo esta usando porque el form sigue enviando post a /login
 		get("/administrador", home::mostrarHomeAdmin, engine);//Si entro como perfil administrador
 		post("/administrador", accesos::cerrarSesion); //Si presiono cerrar sesion durante el uso de la aplicacion
 		post("/administrador/*", accesos::cerrarSesion);
